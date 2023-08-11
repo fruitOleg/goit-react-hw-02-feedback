f50d0ec50bb46f6ac1b303456116706823117ef1
not oleg <104638867+fruitOleg@users.noreply.github.com>
2023-08-11 18:44:30 +0200

    finish

--- src/components/feedback/feedback.Styled.js
+++ src/components/feedback/feedback.Styled.js
@@ -1,28 +1,13 @@
 import styled from 'styled-components';
 
-export const Section = styled.div`
-  margin-top: 150px;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50px;
-  padding: 20px;
-  width: 700px;
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-`;
 export const Title = styled.h1`
   font-size: 40px;
   font-weight: 700;
 `;
-export const List = styled.ul`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  gap: 20px;
-`;
 export const Button = styled.button`
-  width: 100px;
+  margin-left: 10px;
+  margin-right: 10px;
+  width: 80px;
   height: 35px;
   border: 1px;
   border-radius: 8px;
