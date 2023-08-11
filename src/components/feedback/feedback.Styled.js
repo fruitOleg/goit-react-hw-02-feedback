a232e64e92b770b5bdc7764f198be33681afed6e
not oleg <104638867+fruitOleg@users.noreply.github.com>
2023-08-11 18:38:27 +0200

    upd

---
+++ src/components/feedback/feedback.Styled.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Section = styled.div`
+  margin-top: 150px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50px;
+  padding: 20px;
+  width: 700px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
+export const Title = styled.h1`
+  font-size: 40px;
+  font-weight: 700;
+`;
+export const List = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 20px;
+`;
+export const Button = styled.button`
+  width: 100px;
+  height: 35px;
+  border: 1px;
+  border-radius: 8px;
+  box-shadow: 3px 3px 8px #7a8999;
+`;
---
+++ src/components/section/section.styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Sections = styled.div`
+  width: 300px;
+  margin: 20px;
+`;
